0e8a76a58658b3e9cbafdca341bda2c1b9c89c68
Tyler Laprade <pinkmonkeyblue@gmail.com>
2025-08-07 23:26:46 -0400

    Clean up generated files accidentally committed
    Signed-off-by: Tyler Laprade <pinkmonkeyblue@gmail.com>

--- tests/XFAIL/simple_embedding/main.go
+++ tests/XFAIL/simple_embedding/main.go
@@ -20,11 +20,11 @@ func main() {
 		Inner: Inner{Value: 42},
 		Name:  "test",
 	}
-	
+
 	// Direct field access
 	fmt.Println("Value:", o.Value)
 	fmt.Println("Name:", o.Name)
-	
+
 	// Method call
 	fmt.Println("GetValue:", o.GetValue())
 }
