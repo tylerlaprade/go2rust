3f2d3d61a2daaaed48f3d868314a902144794ce1
Tyler Laprade, CFA <pinkmonkeyblue@gmail.com>
2026-05-19 22:45:46 -0400

    Refresh exec command snapshot

--- tests/exec_look_path_not_found/main.rs
+++ tests/exec_look_path_not_found/main.rs
@@ -3,6 +3,87 @@ use std::sync::{Arc, Mutex};
 
 pub mod exec {
     use super::*;
+
+    pub trait GoStringArg {
+        fn into_go_string(self) -> String;
+    }
+
+    impl GoStringArg for String {
+        fn into_go_string(self) -> String {
+            self
+        }
+    }
+
+    impl<'a> GoStringArg for &'a str {
+        fn into_go_string(self) -> String {
+            self.to_string()
+        }
+    }
+
+    impl<'a> GoStringArg for &'a String {
+        fn into_go_string(self) -> String {
+            self.clone()
+        }
+    }
+
+    impl GoStringArg for Arc<Mutex<Option<String>>> {
+        fn into_go_string(self) -> String {
+            self.lock().unwrap().as_ref().cloned().unwrap_or_default()
+        }
+    }
+
+    pub trait GoExecCommandArgs {
+        fn into_exec_args(self) -> Vec<String>;
+    }
+
+    impl GoExecCommandArgs for () {
+        fn into_exec_args(self) -> Vec<String> {
+            Vec::new()
+        }
+    }
+
+    impl GoExecCommandArgs for Arc<Mutex<Option<Vec<String>>>> {
+        fn into_exec_args(self) -> Vec<String> {
+            self.lock().unwrap().as_ref().cloned().unwrap_or_default()
+        }
+    }
+
+    impl<T0: GoStringArg> GoExecCommandArgs for (T0,) {
+        fn into_exec_args(self) -> Vec<String> {
+            vec![self.0.into_go_string()]
+        }
+    }
+
+    impl<T0: GoStringArg, T1: GoStringArg> GoExecCommandArgs for (T0, T1) {
+        fn into_exec_args(self) -> Vec<String> {
+            vec![self.0.into_go_string(), self.1.into_go_string()]
+        }
+    }
+
+    impl<T0: GoStringArg, T1: GoStringArg, T2: GoStringArg> GoExecCommandArgs for (T0, T1, T2) {
+        fn into_exec_args(self) -> Vec<String> {
+            vec![self.0.into_go_string(), self.1.into_go_string(), self.2.into_go_string()]
+        }
+    }
+
+    impl<T0: GoStringArg, T1: GoStringArg, T2: GoStringArg, T3: GoStringArg> GoExecCommandArgs for (T0, T1, T2, T3) {
+        fn into_exec_args(self) -> Vec<String> {
+            vec![self.0.into_go_string(), self.1.into_go_string(), self.2.into_go_string(), self.3.into_go_string()]
+        }
+    }
+
+    impl<T0: GoStringArg, T1: GoStringArg, T2: GoStringArg, T3: GoStringArg, T4: GoStringArg> GoExecCommandArgs for (T0, T1, T2, T3, T4) {
+        fn into_exec_args(self) -> Vec<String> {
+            vec![self.0.into_go_string(), self.1.into_go_string(), self.2.into_go_string(), self.3.into_go_string(), self.4.into_go_string()]
+        }
+    }
+
+    impl<T0: GoStringArg, T1: GoStringArg, T2: GoStringArg, T3: GoStringArg, T4: GoStringArg, T5: GoStringArg> GoExecCommandArgs for (T0, T1, T2, T3, T4, T5) {
+        fn into_exec_args(self) -> Vec<String> {
+            vec![self.0.into_go_string(), self.1.into_go_string(), self.2.into_go_string(), self.3.into_go_string(), self.4.into_go_string(), self.5.into_go_string()]
+        }
+    }
+
     pub fn look_path<T0>(_arg0: T0) -> (Arc<Mutex<Option<String>>>, Arc<Mutex<Option<Box<dyn StdError + Send + Sync>>>>) {
         (Arc::new(Mutex::new(Some::<String>(Default::default()))), Arc::new(Mutex::new(Some::<Box<dyn StdError + Send + Sync>>(Box::<dyn StdError + Send + Sync>::from("executable file not found")))))
     }
